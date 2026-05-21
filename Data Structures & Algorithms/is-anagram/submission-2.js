5e07138ad06d187a2bc4cc387e10832eb1ecf3d9
Soudeep Saha <soudeep.saha.1997@gmail.com>
2026-05-21 08:30:07 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        //let s_array = s.split('');
+        //let t_array = t.split('');
+        if(s.length != t.length) {
+            return false;
+        } 
+
+        let s_array = s.split('');
+        let t_array = t.split('');
+        let s_map = [];
+        let t_map = [];
+
+        for (let i = 0; i < s_array.length; i++) {}
+
+        return false;
+
+    }
+}
