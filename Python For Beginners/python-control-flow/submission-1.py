37375ca3a6ec000307cb03843157f45004a9df85
Soudeep Saha <soudeep.saha.1997@gmail.com>
2026-05-26 08:46:16 +0530

    Add: python-control-flow - submission-1

---
+++ Python For Beginners/python-control-flow/submission-1.py
@@ -0,0 +1,18 @@
+for i in range(1, 8):
+    pass # no execution
+
+if True:
+    pass # no execution
+
+def unfinsished_function():
+    pass # no execution
+
+for i in range(1, 8):
+    break # loop exits
+    print(i)
+
+for i in range(1, 8):
+    continue # no execution and loop continues
+    print(i)
+
+print("nothing else happened")
