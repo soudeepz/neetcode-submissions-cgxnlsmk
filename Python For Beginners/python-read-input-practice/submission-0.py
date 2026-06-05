a2bc12b51c2e919fc0a29a49df04578ae721a653
Soudeep Saha <soudeep.saha.1997@gmail.com>
2026-06-05 12:39:29 +0530

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,14 @@
+def add_two_numbers() -> int:
+    number_string = input()
+    number_list = number_string.split(",")
+    sum = number_list[0] + number_list[1]
+    return sum
+    
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
